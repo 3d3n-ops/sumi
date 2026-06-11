ffc994beb8316fb0b3ec730ed4274bd8ca5ff687
Eden Etuk <3d3n2005@gmail.com>
2026-06-10 23:22:30 -0500

    Initial Commit

---
+++ sumi-ios/sumi_iosApp.swift
@@ -0,0 +1,32 @@
+//
+//  sumi_iosApp.swift
+//  sumi-ios
+//
+//  Created by olumami etuk on 6/10/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct sumi_iosApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ sumi-iosTests/sumi_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  sumi_iosTests.swift
+//  sumi-iosTests
+//
+//  Created by olumami etuk on 6/10/26.
+//
+
+import Testing
+@testable import sumi_ios
+
+struct sumi_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
